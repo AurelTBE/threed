d1a8e6c8388166af20d9902069e7e288f5128206
AurelTBE <aureltrouble@gmail.com>
2022-01-21 21:09:17 +0100

    Fixed type error

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const scene = new THREE.Scene()
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
   const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#BG'),
+    canvas: document.querySelector('#BG') || undefined,
   })
   renderer.setPixelRatio(window.devicePixelRatio)
   renderer.setSize(window.innerWidth, window.innerHeight)
